a8b60132e6900fa05efa09917bd6933779e8fdc8
kin arima <arima@ixam.net>
2021-03-13 16:23:06 +0900

    Create get_an_attachment_file_by_sysid.ps1

---
+++ get_an_attachment_file_by_sysid.ps1
@@ -0,0 +1,25 @@
+# Parameters
+$user = "admin"
+$pass = "admin"
+$instansname = "dev00000"
+$SysID = "8180046a2fba601033e449e72799b680"
+$BasePath = "C:\"
+
+# Build auth header
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $pass)))
+
+# Set proper headers
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add('Authorization',('Basic {0}' -f $base64AuthInfo))
+$headers.Add('Accept','*/*')
+
+# Specify endpoint uri
+$MetaUri = "https://$instansname.service-now.com/api/now/attachment/$SysID"
+$FileUri = "https://$instansname.service-now.com/api/now/attachment/$SysID/file"
+
+# Send HTTP request Get FileName from MetaData
+$response = Invoke-RestMethod -Headers $headers -Method "GET" -Uri $MetaUri
+$FileFullPath = $BasePath + $response.result.file_name
+
+# Send HTTP request GetFile
+Invoke-RestMethod -Headers $headers -Method "GET" -Uri $FileUri -OutFile $FileFullPath
